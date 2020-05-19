e2bcb7d3759fdd3c39de925c27f938353b71bf62
Stackie Jia <jsq2627@gmail.com>
2020-05-19 14:56:33 +0800

    feat: 切换到 brew install node
    经讨论，大家想统一使用 brew 安装 node / yarn。nvm 有很多暗坑

--- init.sh
+++ init.sh
@@ -149,16 +149,10 @@ fi
 
 
 ## nodejs
-if which nvm >/dev/null 2>&1; then
+if which node >/dev/null 2>&1; then
 success "› Skipping nodejs installation"
 else
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-nvm install node
-nvm use node --default
-npm install -g yarn
+brew install node yarn
 fi
 cask_install react-native-debugger
 
